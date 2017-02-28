ff1b012721287a9627c5d8278ecc042135190666
garrettwong <gwong005@gmail.com>
2017-02-27 22:49:40 -0800

    fix datepickers

--- client/app/components/about/about.controller.js
+++ client/app/components/about/about.controller.js
@@ -10,6 +10,39 @@ class AboutController {
 
     model.animationsEnabled = true;
 
+    // dates
+    this.dt = '';
+    this.dt2 = '';
+
+    function disabled(data) {
+      var date = data.date,
+          mode = data.mode;
+      return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+    }
+    
+    this.dateOptions = {
+      dateDisabled: disabled,
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22),
+      minDate: new Date(),
+      startingDay: 1
+    };
+    this.open1 = function() {
+      this.popup1.opened = true;
+    };
+
+    this.open2 = function() {
+      this.popup2.opened = true;
+    };
+
+    this.popup1 = {
+      opened: false
+    };
+
+    this.popup2 = {
+      opened: false
+    };
+
     /*
      * @function open
      * @description opens modal via controller method
