ab15cc1275828c2a42f990822fc2f3a45aecbff8
garrettwong <gwong005@gmail.com>
2017-03-25 17:57:45 -0700

    update iconlist seed

--- client/app/components/iconlist/iconlist.controller.js
+++ client/app/components/iconlist/iconlist.controller.js
@@ -1,8 +1,33 @@
 class IconlistController {
+
+
+  /*
+   * @function constructor
+   * @description
+   * @param listData - a list of objects {color, value, sales, category, title}
+   */
   constructor() {
     this.name = 'iconlist';
 
     console.log(this);
+
+    this.listData = [
+      { 
+        color: 'red', value: 100, sales: 2000, category: 'now', title: 'bryan'
+      },
+      { 
+        color: 'blue', value: 100, sales: 2000, category: 'now', title: 'jackson'
+      },
+      { 
+        color: 'green', value: 100, sales: 2000, category: 'now', title: 'eric'
+      },
+      { 
+        color: 'yellow', value: 100, sales: 2000, category: 'now', title: 'title'
+      },
+      { 
+        color: 'purple', value: 100, sales: 2000, category: 'now', title: 'title'
+      },
+    ]
 
     this.a = 'haasdf';
   }
@@ -11,7 +36,9 @@ class IconlistController {
     console.log(listElement, this.onClick);
     this.selectedListData = listElement;
 
-    this.onClick({ a: this.selectedListData });
+    this.onClick({ 
+      a: this.selectedListData 
+    });
   }
 }
 
