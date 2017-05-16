dc511fbb3d4384a80ac5421d75ef033b25a6de4c
garrettwong <gwong005@gmail.com>
2017-05-16 00:14:48 -0700

    navbar and about fixes

--- client/app/components/about/about.controller.js
+++ client/app/components/about/about.controller.js
@@ -1,5 +1,6 @@
 class AboutController {
   constructor($uibModal, SweetAlert, JsonFileDatabase) {
+
     var model = this;
 
     this.SweetAlert = SweetAlert;
@@ -9,6 +10,9 @@ class AboutController {
     model.items = ['a', 'b', 'c'];
 
     model.animationsEnabled = true;
+
+
+    this.initCollapsePane();
 
 
     /*
@@ -63,6 +67,26 @@ class AboutController {
         
       });
     };
+  }
+
+  initCollapsePane() {
+    $('.collapse-link').on('click', function() {
+        var $BOX_PANEL = $(this).closest('.x_panel'),
+            $ICON = $(this).find('i'),
+            $BOX_CONTENT = $BOX_PANEL.find('.x_content');
+        
+        // fix for some div with hardcoded fix class
+        if ($BOX_PANEL.attr('style')) {
+            $BOX_CONTENT.slideToggle(200, function(){
+                $BOX_PANEL.removeAttr('style');
+            });
+        } else {
+            $BOX_CONTENT.slideToggle(200); 
+            $BOX_PANEL.css('height', 'auto');  
+        }
+
+        $ICON.toggleClass('fa-chevron-up fa-chevron-down');
+    });
   }
 
 }
