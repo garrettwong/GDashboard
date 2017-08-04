e39ca4d01b74ff6f9b9fc2572fef4ace6521aff2
garrettwong <gwong005@gmail.com>
2017-08-04 16:17:12 -0600

    image byte importer

--- client/app/components/admin/users/users.controller.js
+++ client/app/components/admin/users/users.controller.js
@@ -21,6 +21,8 @@ class UsersController {
     this.jsonFileDatabase = JsonFileDatabase;
 
     this.getAll();
+
+    this.initCanvasGetBytes();
   }
 
   /*
@@ -61,6 +63,43 @@ class UsersController {
     this.jsonFileDatabase.remove(this.name, 1).then((response) => {
       console.log('fruit deleted', response.data);
     });
+  }
+
+  getBytes() {
+    var myImage = document.getElementById('myimage');
+    var myCanvas = document.getElementById('mycanvas');
+
+    console.log(myCanvas);
+    var ctx = myCanvas.getContext('2d');
+
+    ctx.drawImage(myImage, 0, 0);
+
+    var mydataURL = myCanvas.toDataURL('image/jpg');
+
+    console.log(mydataURL);
+  }
+
+  initCanvasGetBytes() {
+    var imageLoader = document.getElementById('imageLoader');
+    imageLoader.addEventListener('change', handleImage, false);
+    var canvas = document.getElementById('imageCanvas');
+    var ctx = canvas.getContext('2d');
+
+    function handleImage(e) {
+      var reader = new FileReader();
+      reader.onload = function (event) {
+        var img = new Image();
+        img.onload = function () {
+          canvas.width = img.width;
+          canvas.height = img.height;
+          ctx.drawImage(img, 0, 0);
+        }
+        img.src = event.target.result;
+        console.log('bytes', event.target.result);
+      };
+      reader.readAsDataURL(e.target.files[0]);
+    }
+
   }
 }
 
